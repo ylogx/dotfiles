ba95c96147ee08ca925855e9d4746f689b7102bf
Shubham Chaudhary <me@shubhamchaudhary.in>
2015-07-14 00:04:21 +0530

    Add Mac setup script

---
+++ mac_setup.sh
@@ -0,0 +1,20 @@
+brew install coreutils
+
+brew tap homebrew/dupes
+
+brew install binutils
+brew install diffutils
+brew install ed --default-names
+brew install findutils --with-default-names
+brew install gawk
+brew install gnu-indent --with-default-names
+brew install gnu-sed --with-default-names
+brew install gnu-tar --with-default-names
+brew install gnu-which --with-default-names
+brew install gnutls
+brew install grep --with-default-names
+brew install gzip
+brew install screen
+brew install watch
+brew install wdiff --with-gettext
+brew install wget
