6bcf3aebaf0144b626e662a2ff9881b5b5406f42
Shubham Chaudhary <me@shubhamchaudhary.in>
2017-08-31 20:28:40 +0530

    Install git if necessary

--- install.sh
+++ install.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 set -eu
+install_git_if_needed() {
+    SUDO_IF_NEEDED=''
+    if [[ $EUID > 0 ]]; then
+        SUDO_IF_NEEDED=sudo
+    fi
+    if ! hash git 2>/dev/null; then
+        ${SUDO_IF_NEEDED} apt-get install --yes git
+    fi
+}
 
 HOLDING_LOCATION="${HOME}/.homesick/repos/dotfiles"
 
 if [[ ! -d ${HOLDING_LOCATION} ]]; then
+  install_git_if_needed
   git clone --recursive git://github.com/shubhamchaudhary/dotfiles.git ${HOLDING_LOCATION}
 fi
 cd ${HOLDING_LOCATION}
