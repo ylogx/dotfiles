2ac9ed7229bf45872e84ba0e2bbdad52e50503ff
Shubham Chaudhary <me@shubhamchaudhary.in>
2018-02-06 00:42:00 +0530

    Add lint summary at the end

--- ci/lint.sh
+++ ci/lint.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
-set -eux
+set -eu
+set -o pipefail
 
 if ! hash ansible-lint 2>/dev/null; then
   pip2 install ansible-lint
 fi
 
-ansible-lint playbooks/*.yml
+echo "Running Lint:"
+ansible-lint playbooks/*.yml || ansible-lint playbooks/*.yml | grep ANSIBLE | sort -rn | uniq -c
