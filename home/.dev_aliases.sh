0adb7b3f463b1c4ec7cf7e05271098de8cddd1a7
Shubham Chaudhary <shubham@chaudhary.xyz>
2020-04-29 17:15:14 -0400

    #19 - Remove unused non-generic aliases

--- home/.dev_aliases.sh
+++ home/.dev_aliases.sh
@@ -1,19 +1,3 @@
-#alias cdbuild='cd /home/chaudhary/code/linux/kmix/build'
-#alias cdsrc='cd /home/chaudhary/code/linux/kmix/qmpris'
-#alias cdkg='cd /home/chaudhary/code/linux/kmix/github'
-alias cdde='cd /home/chaudhary/code/linux/kmix/demo/demo'
-alias cdqm='cd /home/chaudhary/code/linux/kmix/qmpris'
-
-##Juk project
-alias cdjuk='cd /home/chaudhary/code/linux/juk'
-alias cdbuild='cd /home/chaudhary/code/linux/juk/build'
-alias mymakejuk='mkdir build; cd ./build && rm -rfv ./*; cmake -DCMAKE_BUILD_PREFIX=$HOME/kde -DCMAKE_BUILD_TYPE=debugfull -DCMAKE_INSTALL_PREFIX=$HOME/kde ../ ; make'
-alias cdlm='cd ~/code/android/studioWorkspace/LazyMusic'
-alias cdlms='cd ~/code/android/studioWorkspace/LazyMusic/app/src/main/java/com/peplet/lazymusic'
-alias cdlmr='cd ~/code/android/studioWorkspace/LazyMusic/app/src/main/res'
-alias cdlo='cd ~/code/android/studioWorkspace/LogMeIn'
-alias cdlos='cd ~/code/android/studioWorkspace/LogMeIn/app/src/main/java/in/shubhamchaudhary/logmein'
-
 #diff-lines() {
     #local path=
     #local line=
